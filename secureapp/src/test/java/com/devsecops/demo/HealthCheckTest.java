fd582aac73a35e8f977fbf72b55177f4858878df
Mirela Schoonbeck <msschoonbeck@gmail.com>
2025-04-21 21:31:01 -0400

    Update secureapp path

---
+++ secureapp/src/test/java/com/devsecops/demo/HealthCheckTest.java
@@ -0,0 +1,12 @@
+package com.devsecops.demo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class HealthCheckTest {
+
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
+}
